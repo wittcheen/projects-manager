843c9159c7f84e77d04d7d85e4ec0770861cf529
Wittchen <wittchenchris@gmail.com>
2025-05-27 14:16:02 +0200

    Created core and error classes

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+from utils.core import Core
+from PySide6.QtWidgets import QMainWindow, QStackedWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Projects Manager")
-
-        self.stack = QStackedWidget()
+        self.stack = QStackedWidget(self)
         self.setCentralWidget(self.stack)
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.resize(600, 400)
-    window.show()
-    sys.exit(app.exec())
+    core = Core(MainWindow)
+    core.run_app()
---
+++ utils/core.py
@@ -0,0 +1,31 @@
+import sys
+from pathlib import Path
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class Core:
+    """ Core application handler. """
+    def __init__(self, main_window: QMainWindow):
+        self.app = QApplication(sys.argv)
+        self.window = main_window()
+
+    def run_app(self):
+        """ Start application event loop. """
+        self.window.resize(600, 400)
+        self._center_window()
+        self.window.show()
+        sys.exit(self.app.exec())
+
+    def _center_window(self):
+        """ Center main window on user's primary screen. """
+        _geometry = self.window.frameGeometry()
+        _center = self.app.primaryScreen().availableGeometry().center()
+        _geometry.moveCenter(_center)
+        self.window.move(_geometry.topLeft())
+
+
+class Error:
+    """ Base class for errors. """
+    class Unexpected(Exception): pass
+    class ServerUnreachable(Exception): pass
+    class MissingCredentials(Exception): pass
+    class InvalidCredentials(Exception): pass
