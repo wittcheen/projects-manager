028a445ad38c6afd23423fd8a578fa76391626d9
Wittchen <wittchenchris@gmail.com>
2025-08-21 14:05:29 +0200

    Added text validator

--- widgets/login_panel.py
+++ widgets/login_panel.py
@@ -2,7 +2,10 @@ from utils.session import FTPSession
 from PySide6.QtWidgets import (
     QWidget, QHBoxLayout, QLineEdit, QLabel, QPushButton, QMessageBox
 )
-from PySide6.QtCore import Qt, Signal
+from PySide6.QtGui import QRegularExpressionValidator
+from PySide6.QtCore import Qt, Signal, QRegularExpression
+
+ALLOWED_CHARS = r"[A-Za-z0-9!?@#$%^&*\-_.]*"
 
 class LoginPanel(QWidget):
     """ Login widget for FTP connections. """
@@ -12,6 +15,7 @@ class LoginPanel(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
         layout = QHBoxLayout(self)
+        validator = QRegularExpressionValidator(QRegularExpression(ALLOWED_CHARS))
 
         self.host_field = QLineEdit(frame = False)
         self.username_field = QLineEdit(frame = False)
@@ -29,6 +33,7 @@ class LoginPanel(QWidget):
         self._fields = []
         for label, field in field_list:
             layout.addWidget(QLabel(label))
+            field.setValidator(validator)
             layout.addWidget(field)
             self._fields.append(field)
 
